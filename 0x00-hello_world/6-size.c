dda04509d753259885b3120a49c839265ffc2d63
Semere2015 <semerenega9.gmail.com>
2023-06-16 03:06:06 -0700

    h.w

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of a char:%zu\n" ,sizeof(char));
-printf("Size of a int:%zu\n" ,sizeof(int));
-printf("Size of a long int:%zu\n" ,sizeof(long int));
-printf("Size of a long long int:%zu\n" ,sizeof(long long int));
-printf("Size of a float:%zu\n" ,sizeof(float));
+printf("Size of a char:%zu\n", sizeof(char));
+printf("Size of a int:%zu\n", sizeof(int));
+printf("Size of a long int:%zu\n", sizeof(long int));
+printf("Size of a long long int:%zu\n", sizeof(long long int));
+printf("Size of a float:%zu\n", sizeof(float));
 return (0);
 }
