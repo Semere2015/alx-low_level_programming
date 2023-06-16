74ca62896d2aef6eeb04cd0d60d32d7990f6b29f
Semere2015 <semerenega9.gmail.com>
2023-06-16 02:30:25 -0700

    h.w

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - size of data types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+sizeof(char);
+sizeof(int);
+sizeof(long int);
+sizeof(long long int);
+sizeof(float);
+return (0);
