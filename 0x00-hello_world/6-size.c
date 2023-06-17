21a0d7e24bcdede3b9c9ae5561943ff55a0eae9f
Semere2015 <semerenega9.gmail.com>
2023-06-17 01:55:41 -0700

    h.w

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
-    printf("Size of char: %zu bytes\n", sizeof(char));
-    printf("Size of int: %zu byte\n", sizeof(int));
-    printf("Size of long int: %zu bytes\n", sizeof(long int));
-    printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-    printf("Size of float: %zu bytes\n", sizeof(float));
-    return (0);
+int main(void)
+{
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu byte\n", sizeof(int));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+printf("Size of float: %zu bytes\n", sizeof(float));
+return (0);
 }
