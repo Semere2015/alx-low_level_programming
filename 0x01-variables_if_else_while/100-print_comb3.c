8b1d32b6935d7a0c652cf074581443641353f3f1
Semere2015 <semerenega9.gmail.com>
2023-06-17 16:44:25 -0700

    v

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*printing numbers with two digit*/
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i <= 8; i++)
+{
+for (j = 1; j <= 9; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
