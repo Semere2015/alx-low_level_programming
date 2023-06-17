3da5340b808d5da37b970645b806026db38e4f86
Semere2015 <semerenega9.gmail.com>
2023-06-17 13:56:42 -0700

    v

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*printing single digit numbers*/
+int main(void)
+{
+int numbers;
+for (numbers = 0; numbers < 10; numbers++)
+{
+printf("%d", numbers);
+}
+printf("\n");
+return (0);
+}
+
