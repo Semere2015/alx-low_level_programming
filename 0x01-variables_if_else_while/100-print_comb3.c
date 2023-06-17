0de3933f3db6a98527a25c51428ee07eaa569e3b
Semere2015 <semerenega9.gmail.com>
2023-06-17 16:58:55 -0700

    v

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,14 +5,20 @@ int main(void)
 {
 int i;
 int j;
+int last_i = 8;
+int last_j = 9;
 for (i = 0; i <= 8; i++)
 {
 for (j = 1; j <= 9; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
+if (i == last_i && j == last_j)
+continue;
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
