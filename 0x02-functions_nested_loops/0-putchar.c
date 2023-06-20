e8ff2a7e5bb7fc1bb61051da35c7f6db31c5948d
Semere2015 <semerenega9.gmail.com>
2023-06-20 11:26:17 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This is the main function of the program. It serves as the entry point
+ * for program execution. The function prints the string "_putchar" to
+ * the standard output using the printf function and returns 0 to indicate
+ * successful execution.
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is my third project in c and it is about loop and functions
