05787b20d6993facb4e6d3115998557889c5a68b
Semere2015 <semerenega9.gmail.com>
2023-06-21 18:47:25 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet in a new line.
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
