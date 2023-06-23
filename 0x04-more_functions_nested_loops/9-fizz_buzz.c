8d85751a8c144aa2d5ea7df4a2058faa288b1c6f
Semere2015 <semerenega9.gmail.com>
2023-06-23 12:51:39 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number from 1 to 100
+ *@i: parameter
+ * Return: 0 succussful
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+return (0);
+}
