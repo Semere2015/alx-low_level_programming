f861f97c0378a61ee04461a14e86efe345b73062
Semere2015 <semerenega9.gmail.com>
2023-06-28 06:52:40 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen - shows length of string
+ * @s: parameter
+ * Return: lenth of string
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
