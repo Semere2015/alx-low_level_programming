e94a91ef2f8562bfae2d218956758f9f24ebe910
Semere2015 <semerenega9.gmail.com>
2023-06-28 05:39:21 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets the value passed from main to 98
+ *@n: pointer that points to the passed variable.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
